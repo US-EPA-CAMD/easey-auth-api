e600c0d65e150b91bc18a152f7b94a8c7fd1da4e
Joshua Mackey <Joshua.Mackey@aplaceformom.com>
2021-06-09 10:14:18 -0400

    Adding empty spec tests to project.

---
+++ src/authentication/authentication.service.spec.ts
@@ -0,0 +1,8 @@
+describe('-- Authentication Service --', () => {
+
+  describe('* authenticate *', () => {
+    it('should authenticate a user from service.', async () => {
+      console.log('testing authenticate service.');
+    });
+  });
+});
---
+++ src/dtos/user.dto.spec.ts
@@ -0,0 +1,8 @@
+describe('-- User DTO --', () => {
+
+  describe('* verify user dto props *', () => {
+    it('should verify user dto properties.', async () => {
+      console.log('testing user dto.');
+    });
+  });
+});
