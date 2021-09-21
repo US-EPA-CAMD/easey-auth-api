e1b63ec2d3a2529803dd8fa67de1d7dc4c51fe33
Kyle Herceg <kdherceg@email.wm.edu>
2021-09-21 13:13:43 -0400

    Move ip to header

--- src/decorators/client-ip.decorator.ts
+++ src/decorators/client-ip.decorator.ts
@@ -4,8 +4,8 @@ export const ClientIP = createParamDecorator(
   (data: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
 
-    if (request.body && request.body.ip) {
-      return request.body.ip;
+    if (request.headers['ip']) {
+      return request.headers['ip'];
     }
 
     return request.ip;
