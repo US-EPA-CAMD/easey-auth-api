52ad8f077cd4fc8e5a5dbeff424c34c3ae666e19
Kyle Herceg <kdherceg@email.wm.edu>
2021-09-21 21:51:28 -0400

    Debug

--- src/decorators/client-ip.decorator.ts
+++ src/decorators/client-ip.decorator.ts
@@ -8,6 +8,11 @@ export const ClientIP = createParamDecorator(
       return request.headers['x-client-ip'];
     }
 
+    console.log(`request.ip: ${request.ip}`);
+    console.log(
+      `request.connection.remoteAddress: ${request.connection.remoteAddress}`,
+    );
+    console.log(`x-forwarded-for: ${request.headers['x-forwarded-for']}`);
     return request.ip;
   },
 );
