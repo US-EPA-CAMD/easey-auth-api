9959cd29f4a029b9e13cc3ed19340af228b7cce3
Kyle Herceg <kdherceg@email.wm.edu>
2021-09-30 12:35:32 -0400

    Changed Auth Guard to default to request.ip

--- src/decorators/client-ip.decorator.ts
+++ src/decorators/client-ip.decorator.ts
@@ -5,13 +5,20 @@ export const ClientIP = createParamDecorator(
     const request = context.switchToHttp().getRequest();
 
     if (request.headers['x-client-ip']) {
-      console.log('X Client IP: ' + request.headers['x-client-ip']);
+      console.log('Using X-Client-IP ' + request.headers['x-client-ip']);
       return request.headers['x-client-ip'];
     }
 
-    console.log('X-Forwarded-For: ' + request.headers['x-forwarded-for']);
+    if (request.headers['x-forwarded-for']) {
+      console.log(
+        'Using X-Forwarded-For ' +
+          request.headers['x-forwarded-for'].split(',')[0],
+      );
+      return request.headers['x-forwarded-for'].split(',')[0];
+    }
 
-    return request.headers['x-forwarded-for'];
+    console.log('Defaulting to IP ' + request.ip);
+    return request.ip;
   },
 );
 
