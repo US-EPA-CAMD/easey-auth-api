46e0a9269e2f39175cdf4ab17e0a6a5ce3746e0a
Kyle Herceg <kdherceg@email.wm.edu>
2021-10-14 10:07:53 -0400

    Changed default bypass to false

--- src/config/bypass.config.ts
+++ src/config/bypass.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('bypass', () => ({
-  bypassed: process.env.EASEY_AUTH_API_BYPASS || true,
+  bypassed: process.env.EASEY_AUTH_API_BYPASS || false,
   pass: process.env.EASEY_AUTH_BYPASS_PASS || 'password',
   users: process.env.EASEY_AUTH_API_BYPASS_USERS || '["kherceg-dp"]',
 }));
