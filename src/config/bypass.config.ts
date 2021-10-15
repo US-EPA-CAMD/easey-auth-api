e80f6f1d2b89008f1528fdd33499720281f3e439
Kyle Herceg <kdherceg@email.wm.edu>
2021-10-14 22:15:08 -0400

    Minor revision deletion

--- src/config/bypass.config.ts
+++ src/config/bypass.config.ts
@@ -3,5 +3,5 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('cdxBypass', () => ({
   enabled: process.env.EASEY_AUTH_API_CDX_BYPASS_ENABLED || false,
   pass: process.env.EASEY_AUTH_API_CDX_BYPASS_PASSWORD || 'IC@nn0tL0g1nIn',
-  users: process.env.EASEY_AUTH_API_CDX_BYPASS_USERS || '["kherceg-dp"]',
+  users: process.env.EASEY_AUTH_API_CDX_BYPASS_USERS || '[]',
 }));
