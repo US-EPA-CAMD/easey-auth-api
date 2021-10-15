56074e48264426f073b1b592880117e835a75567
Kyle Herceg <kdherceg@email.wm.edu>
2021-10-14 23:01:25 -0400

    Another quick fix

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -31,7 +31,7 @@ export class TokenService {
     console.log(this.configService.get<boolean>('cdxBypass.enabled'));
     if (
       this.configService.get<string>('app.env') !== 'production' &&
-      this.configService.get<boolean>('cdxBypass.enabled') === true
+      this.configService.get<string>('cdxBypass.enabled') === 'true'
     ) {
       return true;
     }
