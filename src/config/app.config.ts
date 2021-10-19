6242fcf784de0b574df4438164e535f386735479
Kyle Herceg <kdherceg@email.wm.edu>
2021-10-19 12:19:28 -0400

    feat: rebuild
    Rebuild

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 const path = process.env.EASEY_AUTH_API_PATH || 'api/auth-mgmt';
 const host = process.env.EASEY_AUTH_API_HOST || 'localhost';
 const port = process.env.EASEY_AUTH_API_PORT || 8080;
-
+//
 let uri = `https://${host}/${path}`;
 
 if (host == 'localhost') {
