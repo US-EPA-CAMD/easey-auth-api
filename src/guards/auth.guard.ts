709f726dc2176fc4b194ac03a0e0a43213813ba8
Jason Whitehead <jwhitehead@austin.rr.com>
2022-10-16 14:03:01 -0500

    fixing token refresh issue when CDX expiration has passed

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -6,7 +6,6 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
-import { CurrentUser } from '@us-epa-camd/easey-common/interfaces';
 import { LoggingException } from '@us-epa-camd/easey-common/exceptions';
 
 @Injectable()
@@ -15,7 +14,6 @@ export class AuthGuard implements CanActivate {
 
   async validateRequest(request): Promise<boolean> {
     const authHeader = request.headers.authorization;
-    const forwardedForHeader = request.headers['x-forwarded-for'];
     let errorMsg =
       'Prior Authorization (User Security Token) required to access this resource.';
 
@@ -26,11 +24,6 @@ export class AuthGuard implements CanActivate {
     const splitString = authHeader.split(' ');
     if (splitString.length !== 2 && splitString[0] !== 'Bearer') {
       throw new LoggingException(errorMsg, HttpStatus.BAD_REQUEST);
-    }
-
-    let ip = request.ip;
-    if (forwardedForHeader !== null && forwardedForHeader !== undefined) {
-      ip = forwardedForHeader.split(',')[0];
     }
 
     return true;
