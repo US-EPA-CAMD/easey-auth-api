13f6528b0a20b1c0a1f5b90570cc1777d44f36cb
Kyle-Herceg <kyleherceg@cvpcorp.com>
2023-05-26 10:02:55 -0400

    Added guard code to role retrieval

--- src/permissions/Permissions.service.ts
+++ src/permissions/Permissions.service.ts
@@ -32,7 +32,13 @@ export class PermissionsService {
         });
       })
       .then(res => {
-        if (res && res.length > 0 && res[0]['Role'] && res[0].Role.length > 0) {
+        if (
+          res &&
+          res.length > 0 &&
+          res[0] !== null &&
+          res[0]['Role'] &&
+          res[0].Role.length > 0
+        ) {
           const activeRoles = res[0].Role.filter(
             o => o.status.code === 'Active',
           );
