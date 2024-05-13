3f991b4c9673e4b8a6abf026b3b66a301c90d836
Yonatan Amezene <yamezene@decisionpointcorp.com>
2024-05-13 10:41:27 -0400

    Fixed failing test

--- src/sign/Sign.module.ts
+++ src/sign/Sign.module.ts
@@ -4,7 +4,6 @@ import { ConfigService } from '@nestjs/config';
 import { SignController } from './Sign.controller';
 import { SignService } from './Sign.service';
 import { HttpModule } from '@nestjs/axios';
-import { AuthModule } from '../auth/auth.module';
 import { OidcHelperModule } from '../oidc/OidcHelper.module';
 import { UserSessionModule } from '../user-session/user-session.module';
 import { TokenModule } from '../token/token.module';
--- src/sign/Sign.service.spec.ts
+++ src/sign/Sign.service.spec.ts
@@ -6,12 +6,17 @@ import { CredentialsSignDTO } from '../dtos/certification-sign-param.dto';
 import { SignService } from './Sign.service';
 import { SendPhonePinParamDTO } from '../dtos/send-phone-pin-param.dto';
 import { CurrentUser } from '@us-epa-camd/easey-common/interfaces';
-import { UserSessionService } from '../user-session/user-session.service';
 import { TokenService } from '../token/token.service';
 import { OidcHelperService } from '../oidc/OidcHelperService';
 import { OidcHelperModule } from '../oidc/OidcHelper.module';
 import { UserSessionModule } from '../user-session/user-session.module';
 import { TokenModule } from '../token/token.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserSessionRepository } from '../user-session/user-session.repository';
+import { UserSession } from '../entities/user-session.entity';
+import { ClientTokenRepository } from '../client-token/client-token.repository';
+import { UserSessionService } from '../user-session/user-session.service';
+import { SignAuthResponseDTO } from '../dtos/sign-auth-response.dto';
 
 const client = {
   AuthenticateAsync: jest.fn().mockResolvedValue([{ securityToken: '' }]),
@@ -33,13 +38,39 @@ jest.mock('soap', () => ({
   createClientAsync: jest.fn(() => Promise.resolve(client)),
 }));
 
+const mockUserSessionService = () => ({
+  findSessionByUserId: jest.fn().mockResolvedValue(new UserSession() ),
+});
+
+const mockOidcHelperService = () => ({
+  makePostRequestForFile: jest.fn().mockResolvedValue(new SignAuthResponseDTO() ),
+  makePostRequestJson: jest.fn().mockResolvedValue(new SignAuthResponseDTO() ),
+});
+
+const mockTokenService = () => ({
+  validateClientIp: jest.fn(),
+});
+
 describe('SignService', () => {
   let service: SignService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [LoggerModule, OidcHelperModule, UserSessionModule, TokenModule],
-      providers: [SignService, ConfigService],
+      imports: [LoggerModule],
+      providers: [SignService, ConfigService,
+        {
+          provide: UserSessionService,
+          useValue: mockUserSessionService,
+        },
+        {
+          provide: OidcHelperService,
+          useValue: mockOidcHelperService,
+        },
+        {
+          provide: TokenService,
+          useValue: mockTokenService,
+        },
+      ],
     }).compile();
 
     service = module.get<SignService>(SignService);
