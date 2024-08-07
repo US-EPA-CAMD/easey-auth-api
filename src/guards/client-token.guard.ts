d4c95ce173bafb4211ca40005cf060d83477beeb
Jon Maxwell Diebold <max.diebold@erg.com>
2024-08-07 18:39:01 -0400

    fix(6290): updated to match version of easey-common

--- src/guards/client-token.guard.ts
+++ src/guards/client-token.guard.ts
@@ -1,15 +1,15 @@
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 import {
   Injectable,
   CanActivate,
   ExecutionContext,
   HttpStatus,
-} from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
-import { LoggingException } from '@us-epa-camd/easey-common/exceptions';
+import { EaseyException } from '@us-epa-camd/easey-common/exceptions';
 
-import { ClientTokenService } from "src/client-token/client-token.service";
+import { ClientTokenService } from 'src/client-token/client-token.service';
 
 @Injectable()
 export class ClientTokenGuard implements CanActivate {
@@ -21,26 +21,30 @@ export class ClientTokenGuard implements CanActivate {
   async validateRequest(request): Promise<boolean> {
     const clientId = request.body.clientId;
     const authHeader = request.headers.authorization;
-    const errorMsg = "Client Id and Token are required to access this resource.";
+    const errorMsg =
+      'Client Id and Token are required to access this resource.';
 
-    if (clientId === null || clientId === undefined ||
-        authHeader === null || authHeader === undefined
+    if (
+      clientId === null ||
+      clientId === undefined ||
+      authHeader === null ||
+      authHeader === undefined
     ) {
-      throw new LoggingException(errorMsg, HttpStatus.BAD_REQUEST);
+      throw new EaseyException(new Error(errorMsg), HttpStatus.BAD_REQUEST);
     }
 
-    const splitString = authHeader.split(" ");
-    if (splitString.length !== 2 && splitString[0] !== "Bearer") {
-      throw new LoggingException(errorMsg, HttpStatus.BAD_REQUEST);
+    const splitString = authHeader.split(' ');
+    if (splitString.length !== 2 && splitString[0] !== 'Bearer') {
+      throw new EaseyException(new Error(errorMsg), HttpStatus.BAD_REQUEST);
     }
 
     return this.tokenService.validateToken(clientId, splitString[1]);
   }
 
   canActivate(
-    context: ExecutionContext
+    context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    if (this.configService.get("app.enableClientToken") === true) {
+    if (this.configService.get('app.enableClientToken') === true) {
       const request = context.switchToHttp().getRequest();
       return this.validateRequest(request);
     }
@@ -48,3 +52,4 @@ export class ClientTokenGuard implements CanActivate {
     return true;
   }
 }
+
